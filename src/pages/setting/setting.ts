5cb66785679a13af2ae8b7485f9e96b9f69d6106
jonasaac <jonasaavedra@udec.cl>
2018-10-31 16:29:26 -0300

    Se crea página para comentarios de posibles usuarios

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AlertController} from "ionic-angular";
+import {isUndefined} from "ionic-angular/util/util";
 
 /**
  * Generated class for the SettingPage page.
@@ -14,12 +16,90 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'setting.html',
 })
 export class SettingPage {
+  state : any = 0;
+  datos : any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl : AlertController) {
+    this.datos = {
+      Comentario: this.Comenta,
+      Correo: this.Correo
+    };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingPage');
+  }
+
+  comentario_enviado(){
+    console.log(this.Comenta);
+    console.log(this.Correo);
+    this.state = 0;
+    try {
+      if (this.Correo.search("@") > 0) {
+        console.log("Correo correcto");
+      }
+      else {
+        console.log("Correo debe ingresarse otra vez");
+        this.alerta_correo();
+        this.state = 1;
+      }
+    }
+    catch{
+      console.log("No se ha ingresado correo");
+      this.alerta_correo();
+      this.state = 1;
+    }
+    if(isUndefined(this.Comenta)){
+      this.alerta_comentario()
+      this.state = 1;
+    }
+    if(this.state === 0){
+      console.log("Sin problema");
+    }
+    else{
+      console.log("Algo falta");
+    }
+
+
+  }
+
+  alerta(){
+    const alert = this.alertCtrl.create(
+      {
+        title : "Error!",
+        subTitle : "Se debe ingresar comentario y correo para continuar",
+        buttons : ["Cancelar"]
+      }
+    );
+    alert.present();
+  }
+
+  alerta_correo(){
+    const alert = this.alertCtrl.create(
+      {
+        title : "Error!",
+        subTitle : "Se debe ingresar un correo válido",
+        buttons : ["Cancelar"]
+      }
+    );
+    alert.present();
+  }
+
+  alerta_comentario(){
+    const alert = this.alertCtrl.create(
+      {
+        title : "Error!",
+        subTitle : "Se debe ingresar comentario",
+        buttons : ["Cancelar"]
+      }
+    );
+    alert.present();
+  }
+
+  limpiar_comentario(){
+    this.Comenta = "";
+    this.Correo = "";
+
   }
 
 }
